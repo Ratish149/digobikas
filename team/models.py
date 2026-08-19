24921cfdcded49c814ced6ac55fb7f5e0aa3a231
Ratish149 <ratish.shakya149@gmail.com>
2026-08-19 10:30:59 +0545

    feat: add formal_staff option to TeamMember model types

--- team/models.py
+++ team/models.py
@@ -9,6 +9,7 @@ class TeamMember(TimeStampedModel):
     MEMBER_TYPE_CHOICE = (
         ("board_member", "Board Member"),
         ("staff", "Staff"),
+        ("formal_staff", "Formal Staff"),
     )
     GENDER_CHOICE = (
         ("male", "Male"),
