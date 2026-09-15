9279995a2ce2cbda39a5dd09e1268b24c6f40cf8
Ratish149 <ratish.shakya149@gmail.com>
2026-09-15 14:44:00 +0545

    feat: add order to list_editable in TeamMemberAdmin

--- team/admin.py
+++ team/admin.py
@@ -7,6 +7,7 @@ from team.models import TeamMember
 @admin.register(TeamMember)
 class TeamMemberAdmin(ModelAdmin):
     list_display = ["name", "member_type", "designation", "order", "slug", "created_at"]
+    list_editable = ["order"]
     list_filter = ["member_type"]
     search_fields = ["name", "designation"]
     prepopulated_fields = {"slug": ("name",)}
